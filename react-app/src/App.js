228d75d1b5f87c833cee03f642f34aecd8df59b4
Harshith Gowda <harshithgowdakt@gmail.com>
2023-06-14 17:04:41 +0530

    added example of useEffect

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,17 +1,25 @@
 import './App.css'
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
+
 function App() {
   const [emotion, setEmotion] = useState("happy");
+  const [secodary, setSecondary] = useState("tired");
 
   useEffect(() => {
     console.log(`It's ${emotion} right now`);
-  }, [emotion])
+  }, [emotion]);
+
+  useEffect(() => {
+    console.log(`It's ${secodary} right now`)
+  });
 
   return (
     <div className="App">
       <h1>Current emotion is {emotion}</h1>
       <button onClick={() => setEmotion("sad")}> sad</button>
       <button onClick={() => setEmotion("excited")}> excited</button>
+      <h1>Secondary emotion is {secodary}</h1>
+      <button onClick={() => setSecondary("grateful")}> grateful</button>
     </div>
   );
 }
