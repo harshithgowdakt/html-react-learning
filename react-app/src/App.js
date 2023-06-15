cb576f33842f52c950371dc59d08cea2c741a367
Harshith Gowda <harshithgowdakt@gmail.com>
2023-06-15 11:09:39 +0530

    added links

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,8 +1,14 @@
 import './App.css'
+import { Link } from "react-router-dom"
 
 export function Home() {
   return (
     <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">about</Link>
+        <Link to="/contact">contact</Link>
+      </nav>
       <h1>my website</h1>
     </div>
   )
@@ -11,6 +17,11 @@ export function Home() {
 export function About() {
   return (
     <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">about</Link>
+        <Link to="/contact">contact</Link>
+      </nav>
       <h1>about us</h1>
     </div>
   )
@@ -19,6 +30,11 @@ export function About() {
 export function Contact() {
   return (
     <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">about</Link>
+        <Link to="/contact">contact</Link>
+      </nav>
       <h1> contact us</h1>
     </div>
   )
